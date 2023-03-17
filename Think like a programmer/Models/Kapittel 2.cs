7488d123776023a458212dd89bcd3018063c647d
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-03-17 07:40:22 +0100

    Add project files.

---
+++ Think like a programmer/Models/Kapittel 2.cs
@@ -0,0 +1,72 @@
+﻿namespace Think_like_a_programmer.Models
+{
+    internal class Kapittel_2
+    {
+        private List<List<int>> Oppgave1 = new List<List<int>>() {
+            new List<int>() { 8 },
+            new List<int>() { 0, 1, 6 },
+            new List<int>() { 0, 2, 4 },
+            new List<int>() { 0, 3, 2 },
+        };
+
+        private List<List<int>> Oppgave2 = new List<List<int>>() {
+            new List<int>() { 0, 3, 2 },
+            new List<int>() { 0, 2, 4 },
+            new List<int>() { 0, 1, 6 },
+            new List<int>() { 8 },
+            new List<int>() { 0, 1, 6 },
+            new List<int>() { 0, 2, 4 },
+            new List<int>() { 0, 3, 2 },
+        };
+
+        private List<List<int>> Oppgave3 = new List<List<int>>() {
+            new List<int>() { 1, 12, 1 },
+            new List<int>() { 0, 1, 2, 8, 1, 2 },
+            new List<int>() { 0, 2, 3, 4, 3 },
+            new List<int>() { 0, 3, 8 },
+            new List<int>() { 0, 3, 8 },
+            new List<int>() { 0, 2, 3, 4, 3 },
+            new List<int>() { 0, 1, 2, 8, 1, 2 },
+            new List<int>() { 1, 12, 1 },
+        };
+
+
+        public void SelectShape()
+        {
+            PrintShape(Oppgave1);
+
+            Console.WriteLine();
+
+            PrintShape(Oppgave2);
+
+            Console.WriteLine();
+
+            PrintShape(Oppgave3);
+
+        }
+
+        private void PrintShape(List<List<int>> Oppgave)
+        {
+            foreach (var list in Oppgave)
+            {
+                PrintLine(list);
+            }
+        }
+
+        private void PrintLine(List<int> list)
+        {
+            for (var i = 0; i < list.Count; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    for (var j = 0; j < list[i]; j++) Console.Write("#");
+                }
+                else
+                {
+                    for (var j = 0; j < list[i]; j++) Console.Write(" ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Think like a programmer/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Think_like_a_programmer.Models;
+
+namespace programmet
+{
+    class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Kapittel_2 test = new Kapittel_2();
+            test.SelectShape();
+        }
+    }
+}
